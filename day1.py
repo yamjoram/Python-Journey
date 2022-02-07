25c445ec8fd45e0c2a3c94fb270f4826c3f1bdd5
yamjoram <99007890+yamjoram@users.noreply.github.com>
2022-02-08 01:27:44 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,87 @@
+
+
+
+print("hello world!")
+print('hello world!')
+#chapter2
+#print('i like doing it') # is used to disable a comment
+
+#chapter3
+print("i will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("roosters", 100 - 25 * 3 % 4)
+
+print("Now i will count the eggs")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 > 5 - 7 ?")
+print(3 + 2 > 5 -7)
+print("add this two no,", 3 + 4)
+print("answer it", 7 + 1 >= 3 * 7)
+
+print("*******************\n")
+# chapter 4
+cars = 100
+space_in_a_car = 4
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers #70
+cars_driven = drivers #30
+carpool_capacity = cars_driven * space_in_a_car #120
+average_passengers_per_car = passengers / cars_driven #3
+
+print("there are", cars, "cars available")
+print("There are only", drivers, "drivers available")
+print("There will be", cars_not_driven, "empty cars today")
+print("We can transport", carpool_capacity, "people today")
+print("We have", passengers, "to carpool today")
+print("We need to put about", average_passengers_per_car, "in each car.")
+print("********************\n")
+#chapter5
+my_name = "Joram yam"
+my_age = 24
+my_height = 153
+my_weight = 47
+my_eyes = "black"
+my_hair = "short"
+
+print(f"let's talk about {my_name}")
+print(f"he's {my_height} cm tall.")
+print(f"he's {my_weight} kg")
+print(f"He's has {my_eyes} eyes and {my_hair} hair.")
+
+print("**************************\n")
+#chapter  6
+type_of_people = 10
+x = f"There are {type_of_people} type of people"
+
+binary = "binary"
+do_not = "don't"
+y = f"people who know {binary} and people who {do_not}"
+print(x)
+print(y)
+
+print(f"I said, {x}")
+print(f"I said, {y}")
+
+print("************************\n")
+#chapter7
+# .format() dot - i want to run a command on that string
+
+print("Mary had a little lamb")
+print("It's fleece was white as {}.".format("snow"))
+print("." * 10)
+
+end1 = "J"
+end2 = "o"
+end3 = "r"
+end4 = "a"
+end5 = "m"
+end6 = "y"
+end7 = "a"
+end8 = "m"
+print(end1 + end2 + end3 + end4 + end5)
+print(end6 + end7 + end8)
+
+print("************\n")
