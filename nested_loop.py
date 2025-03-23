e66fbea65715a614de23ac9c00b826195ca0ca63
yamjoram <99007890+yamjoram@users.noreply.github.com>
2025-03-24 00:13:04 +0530

    nested_loop.py

---
+++ nested_loop.py
@@ -0,0 +1,14 @@
+for i in range(1,6):
+    print(f"Pass {i}-", end = " ")
+    for j in range(1,6):
+        print(j, end = " ")
+    print()
+
+
+$####################################
+
+for i in range(1,6):
+    print("*", end = "")
+    for j in range(1,11):
+        print("*", end = "")
+    print()
